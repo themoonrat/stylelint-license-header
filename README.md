a8aabc674695409919723689c117258fab9c04e9
David Moore <davimoore@microsoft.com>
2024-07-05 17:38:11 +0100

    Ensures that only comments at the top of the file can get caught as an error

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# stylelint-license-header-top
+# stylelint-license-header
 
 A [Stylelint](https://stylelint.io/) plugin to enforce (and fix/update) a required header comment for license/copyright information in CSS files.
 
@@ -9,11 +9,11 @@ Follow instructions for installation and setup of [Stylelint](https://stylelint.
 Then install this plugin with yarn or npm:
 
 ```
-yarn add --dev stylelint-license-header-top
+yarn add --dev stylelint-license-header
 ```
 
 ```
-npm add --save-dev stylelint-license-header-top
+npm add --save-dev stylelint-license-header
 ```
 
 ## Setup
@@ -23,16 +23,19 @@ Set up in `.stylelintrc`:
 ```
 {
   "plugins": [
-    "stylelint-license-header-top"
+    "@themoonrat/stylelint-license-header"
   ],
   "rules": {
-    "license-header-top": [true, {
-      "license": "Copyright 2020-2024 MyCompany Inc."
-    }]
+    "license-header/license-header": [
+	  true,
+	  {
+        "license": "Copyright 2020-2024 MyCompany Inc."
+      }
+    ]
   }
 }
 ```
 
-Pass a *string* into the `license` option. This string will be placed into a comment (`/* yourStringHere */`) and compared against the first comment of each linted file.
+Pass a *string* into the `license` option. This string will be placed into a comment (`/* yourStringHere */`) and compared against the top of each linted file.
 
 This plugin supports autofixing. When you pass the `--fix` option to Stylelint, the given license header will be added at the top of any files that are missing it.
--- index.js
+++ index.js
@@ -1,74 +1,85 @@
-const stylelint = require('stylelint')
+const stylelint = require("stylelint");
 
-const ruleName = 'kuali/license-header'
+const ruleName = "license-header/license-header";
 const messages = stylelint.utils.ruleMessages(ruleName, {
-  rejected: 'Missing or incorrect license header',
-})
+	missing: "Missing license header",
+	incorrect: "Incorrect license header",
+});
 
 module.exports = stylelint.createPlugin(ruleName, (actual, options = {}, context) => {
-  return (root, result) => {
-    const validOptions = stylelint.utils.validateOptions(
-      result,
-      ruleName,
-      { actual },
-      {
-        actual: options,
-        possible: {
-          license: v => typeof v === 'string'
-        },
-        optional: false
-      }
-    )
+	return (root, result) => {
+		const validOptions = stylelint.utils.validateOptions(
+			result,
+			ruleName,
+			{ actual },
+			{
+				actual: options,
+				possible: {
+					license: (v) => typeof v === "string",
+				},
+				optional: false,
+			},
+		);
 
-    if (!validOptions) {
-      return
-    }
+		if (!validOptions) {
+			return;
+		}
 
-    const disableFix = options.disableFix || false
-    const isFixEnabled = context.fix && !disableFix
-    let isLicenseHeaderPresent = false
+		const disableFix = options.disableFix || false;
+		const isFixEnabled = context.fix && !disableFix;
 
-    root.walkComments(comment => {
-      commentIsFirst = comment === comment.parent.first
+		const firstNode = root.first;
 
-      if (commentIsFirst) {
-        const commentText = comment.text.trim()
-        const licenseText = options.license.trim()
-        if (commentText === licenseText) {
-          isLicenseHeaderPresent = true
-          return
-        } else {
-          if (isFixEnabled) {
-            // Don't report, fix will be applied
-            comment.remove()
-          } else {
-            stylelint.utils.report({
-              message: messages.rejected,
-              node: comment,
-              result,
-              ruleName,
-            })
-          }
-        }
-      }
-    })
+		if (firstNode && firstNode.type === "comment") {
+			const commentText = firstNode.text.trim();
+			const licenseText = options.license.trim();
 
-    if (!isLicenseHeaderPresent && isFixEnabled) {
-      const newline = context.newline
+			if (commentText === licenseText) {
+				// License header is correct
+				return;
+			} else {
+				if (isFixEnabled) {
+					// Don't report, fix will be applied
+					firstNode.remove();
+				} else {
+					stylelint.utils.report({
+						message: messages.incorrect,
+						node: firstNode,
+						result,
+						ruleName,
+					});
+				}
+			}
+		} else {
+			// No comment found at the top
+			if (!isFixEnabled) {
+				stylelint.utils.report({
+					message: messages.missing,
+					node: root,
+					result,
+					ruleName,
+				});
+			}
+		}
 
-      // Add license header comment
-      const licenseComment = `/*${options.license}*/`
-      root.prepend(licenseComment)
+		if (!isFixEnabled) {
+			return;
+		}
 
-      // Add space before next block
-      const nextChild = root.nodes[1]
-      if (nextChild) {
-        existingBefore = nextChild.raws.before || ''
-        nextChild.raws.before = newline + newline + existingBefore 
-      }
-    }
-  }
-})
+		// Add license header comment
+		const licenseComment = `/*${options.license}*/`;
+		root.prepend(licenseComment);
 
-module.exports.ruleName = ruleName
-module.exports.messages = messages
+		// Add space before next block
+		const nextChild = root.nodes[1];
+		if (nextChild) {
+			const newline = context.newline;
+
+			existingBefore = nextChild.raws.before || "";
+			nextChild.raws.before = newline + newline + existingBefore;
+		}
+	};
+});
+
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
